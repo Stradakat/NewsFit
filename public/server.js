d039c0eebb6d544acdd9141f1974ca2d3e4d26e9
Rick Felix <Felirick@gmail.com>
2017-08-25 16:02:12 -0700

    Heroku Launch Version

--- public/server.js
+++ public/server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var logger = require('morgan');
+ var logger = require('morgan');
 var mongoose = require('mongoose');
 // Notice: Our scraping tools are prepared, too
 var request = require('request'); 
